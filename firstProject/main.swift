146358286668a57fa1f09dfb1e154a85ece245d7
Ka Lam Ho <ka.l.ho-1@student.uts.edu.au>
2019-03-18 19:34:23 +1100

    first commit

---
+++ firstProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  firstProject
+//
+//  Created by Mac on 18/3/2019.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+import Foundation
+// test 1  
+print("Hello, World!")
+
